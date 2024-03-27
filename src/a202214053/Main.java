360775b2ecf19ca007bd705170d24022721722a8
mini8455 <miniminhi04@gmail.com>
2024-03-27 12:24:07 +0900

    sixth

--- src/a202214053/Main.java
+++ src/a202214053/Main.java
@@ -3,8 +3,9 @@ package a202214053;
 public class Main {
 	
 	public static void main(String[]args) {
-		System.out.println("í™ê¸¸ë™");
-		System.out.println("í•˜ë‚˜");
+		System.out.println("ÀÌ¹ÎÈñ");
+		System.out.println("ÇÏ³ª");
+		System.out.println("µÑ");
 	}
 	
 }
